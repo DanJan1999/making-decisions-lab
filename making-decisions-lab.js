6eddbb4996397917d425a69b03534ca89ce9fede
Daniel Janis <danieljanis99@gmail.com>
2021-08-10 12:23:04 -0600

    Making Decisions Lab Completed

--- making-decisions-lab.js
+++ making-decisions-lab.js
@@ -8,6 +8,14 @@
 
 //CODE HERE
 
+var lovesCode = true
+
+if (lovesCode === true) {
+  console.log(`I love to code`)
+} else {
+  console.log(`Coding has it's challenges`)
+}
+
 // For problems 2-3 use the following lines of code:
 var amysAge = 29
 var brittanisAge = 34
@@ -21,7 +29,14 @@ var brittanisBirthYear = 1986
 */
 
 //CODE HERE
-
+if (amysAge > brittanisAge){
+  console.log(`Amy is older`)
+}
+else if (brittanisAge > amysAge){
+  console.log(`Brittani is older`)
+}else{
+  console.log(`Brittani and Amy are the same age`)
+}
 
 ////////// PROBLEM 3 //////////
 
@@ -31,7 +46,11 @@ var brittanisBirthYear = 1986
 
 //CODE HERE
 
-
+if (amysBirthYear === brittanisBirthYear) {
+  console.log(`Amy and Brittani were born in the same year`)
+} else {
+  console.log(`Amy and Brittani were not born in the same year`)
+}
 
 // For problems 4-5 use the following lines of code:
 var arr = [10,20,30,40,50,60]
@@ -42,7 +61,8 @@ var arr = [10,20,30,40,50,60]
 */
 
 //Code Here
-
+let firstItem = arr[0]
+console.log(firstItem)
 
 ////////// PROBLEM 5 //////////
 /*
@@ -50,6 +70,10 @@ var arr = [10,20,30,40,50,60]
 */
 
 //Code Here
+
+let lastItemRemoved = arr.splice(5, 1)
+console.log(arr)
+console.log(lastItemRemoved)
 
 ////////// PROBLEM 6 //////////
 
@@ -62,7 +86,9 @@ var family = ['Tyler', 'Jordan', 'Ryan', 'Alice', 'Ireland'];
 */
 
 //Code Here
-
+for(i=0; i < family.length; i++){
+  console.log(family[i])
+}
 
 
 ////////// PROBLEM 7 //////////
@@ -78,7 +104,12 @@ let evensArr = []
 
 //Code Here
 
-
+for (i = 0; i < nums.length; i++) {
+  if (nums[i]%2 === 0) {
+    evensArr.push(nums[i])
+  }
+}
+console.log(nums, evensArr)
 
 ////////// PROBLEM 8 //////////
 
@@ -91,7 +122,21 @@ var score = 74
 */
 
 //Code Here
-
+if(score >= 90){
+  console.log(`A`)
+}
+else if (score >= 80 && score <= 89){
+  console.log(`B`)
+}
+else if (score >= 70 && score<=79){
+  console.log(`C`)
+}
+else if (score >= 60 && score<=69){
+  console.log(`D`)
+}
+else{
+  console.log(`F`)
+}
 
 ////////// Intermediate Problems //////////
 
@@ -104,12 +149,22 @@ var score = 74
 
 //CODE HERE
 
+let changeMyMind = true
+console.log(changeMyMind)
+if (changeMyMind === true) {
+  changeMyMind = false
+} else {
+  changeMyMind = true
+}
+console.log(changeMyMind)
 
 // Try to change the value of changeMyMind (irrespective of whether it is true or false) and then console.log it's new status. It should log true. Hint: use the not operator.
 
 //CODE HERE
-
-
+if(changeMyMind !== true){
+  changeMyMind = true
+}
+console.log(changeMyMind)
 
 
 ////////// PROBLEM 10 //////////
@@ -124,13 +179,21 @@ var myFavoriteNumbers = [4,8,12,16,20,24];
 
 //Code Here
 
-
+let someNum = myFavoriteNumbers[4]
+console.log(someNum)
 
 // Subscripting (accessing values using their index) an array can fail. What happens if you subscript to the 7th element, but there are not 7 elements in the array? Let's write some code to check for that. 
 // Use an if statement to check the length of the 'myFavoriteNumbers' array. If it's less than 7, log 'There are not enough elements in this array' to the console. If the length is more than 7, reassign the value of 'someNum' to the value of the 7th element in the array. (Hint: how can you make sure that your code works for exactly 7 elements? What index do you use to get the 7th element?)
 
 //Code Here
 
+//myFavoriteNumbers.push(15)
+if (myFavoriteNumbers.length < 7) {
+  console.log(`There are not enough elements in this array`)
+} else {
+  someNum = myFavoriteNumbers[6]
+}
+console.log(someNum)
 
 ////////// PROBLEM 11 //////////
 
@@ -141,7 +204,11 @@ var listOfNumbers = [1,2,3,4,5,6,7,8,9,10,11,12];
 // Use a for-loop to iterate through 'listOfNumbers', checking to see if each number is divisible by 3. If it is, console.log '{number} is divisible by 3.'
 
 //Code Here
-
+for(i=0;i < listOfNumbers.length; i++){
+  if(listOfNumbers[i] % 3 === 0){
+    console.log(`${listOfNumbers[i]} is divisible by 3`)
+  }
+}
 
 ////////// PROBLEM 12 //////////
 // Do not edit the code below.
@@ -154,6 +221,9 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 
 //Code Here
 
+for (i = letters.length - 1; i >=0; i--) {
+  console.log(letters[i])
+}
 
 ////////// Advanced Problems //////////
 
@@ -176,6 +246,26 @@ If the letter grade is not one of the above letters, console.log 'Not an eligibl
 
 //Code Here
 
+// letterGrade = `W`
+switch(letterGrade){
+  case 'A':
+  console.log("The student is doing excellently.")
+  break;
+  case 'B':
+  console.log("The student is doing well.")
+  break;
+   case 'C':
+  console.log("The student is doing alright.")
+  break;
+   case 'D':
+  console.log("The student is not doing very well.")
+  break;
+   case 'F':
+  console.log("The student is failing.")
+  break;
+  default:
+  console.log("Not an eligible grade.")
+}
 
 
 ////////// PROBLEM 14 //////////
@@ -203,3 +293,15 @@ If the letter grade is not one of the above letters, console.log 'Not an eligibl
 */
 
 //Code Here
+
+for (i = 1; i <=100; i++) {
+  if (i % 5 === 0 && i % 3 === 0) {
+    console.log(`DevMountain`)
+  } else if (i % 5 === 0 && i % 3 !== 0) {
+    console.log(`Mountain`)
+  } else if (i % 5 !== 0 && i % 3 === 0) {
+    console.log(`Dev`)
+  } else {
+    console.log(i)
+  }
+}
